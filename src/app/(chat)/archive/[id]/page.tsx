b0a9a8b26e604c9cbffb6bc1403ad23324e81581
AhmedBenAbdallahDev <celiandorra@gmail.com>
2026-03-18 09:50:27 +0100

    feat: enhance visual effects by adding overflow hidden to background elements

--- src/app/(chat)/archive/[id]/page.tsx
+++ src/app/(chat)/archive/[id]/page.tsx
@@ -88,23 +88,23 @@ export default async function ArchivePage({
   return (
     <>
       <>
-        <div className="absolute opacity-30 pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000">
+        <div className="absolute opacity-30 pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000 overflow-hidden">
           <LightRays className="bg-transparent" />
         </div>
-        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000">
+        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000 overflow-hidden">
           <Particles
             className="bg-transparent"
             particleCount={400}
             particleBaseSize={10}
           />
         </div>
-        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000">
+        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000 overflow-hidden">
           <div className="w-full h-full bg-gradient-to-t from-background to-50% to-transparent z-20" />
         </div>
-        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000">
+        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000 overflow-hidden">
           <div className="w-full h-full bg-gradient-to-l from-background to-20% to-transparent z-20" />
         </div>
-        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000">
+        <div className="absolute pointer-events-none top-0 left-0 w-full h-full z-10 fade-in animate-in duration-5000 overflow-hidden">
           <div className="w-full h-full bg-gradient-to-r from-background to-20% to-transparent z-20" />
         </div>
       </>

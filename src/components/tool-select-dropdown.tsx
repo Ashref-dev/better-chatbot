df66f5540398dd776959ec3c7504cc37f89cee81
carthageadev <celiandorra@gmail.com>
2026-07-27 12:23:03 +0100

    fix: use horizontal padding on tools label

--- src/components/tool-select-dropdown.tsx
+++ src/components/tool-select-dropdown.tsx
@@ -199,7 +199,7 @@ export function ToolSelectDropdown({
       >
         <span
           className={cn(
-            "hidden pl-1 sm:inline-flex",
+            "hidden px-0.5 sm:inline-flex",
             !bindingTools.length && "text-muted-foreground",
           )}
         >
